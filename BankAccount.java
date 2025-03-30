d3888816e5809eaae33dc0cd69cc0f4fab8f9f09
EDAKULA PRATHUSH KUMAR <193083719+Prathush-Kumar@users.noreply.github.com>
2025-03-30 14:35:16 +0530

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -1,5 +1,5 @@
 // Abstract class BankAccount that defines the basic structure for all bank accounts
-
+ 
 public abstract class BankAccount {
     protected String accountID = "0000-0000-0000-0000"; // Default account ID
     protected double interestRate = 0.0; // Default interest rate
